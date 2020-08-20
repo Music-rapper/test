f9c129cf866a2cda43940ebf5d845069adbb35fe
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-20 16:09:24 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -9,41 +9,6 @@ Bot = commands.Bot(command_prefix = '-')
 @Bot.command()
 async def hello(ctx):
     	await ctx.send(f"Hello {ctx.author.mention}")
-
-@Bot.command()
-async def say(ctx, *, text):
-	await ctx.send(text)
-	
-
-		
-@Bot.command()
-async def ping(ctx):
-    	channel = ctx.channel
-    	t1 = time.perf_counter()
-    	await channel.trigger_typing()
-    	t2 = time.perf_counter()
-    	latency = round(Bot.latency *1000)
-    	t = round((t2-t1)*1000)
-    	green = discord.Color.green()
-    	desc=f":heartbeat: **{latency}**ms \n :stopwatch: **{t}**ms"
-    	em = discord.Embed(title = ":ping_pong: Pong",description = desc, color = green)
-    	em.set_footer(text = f"Requested by {ctx.author.name}",icon_url=ctx.author.avatar_url)
-    	await ctx.send(embed = em)
-	
-@Bot.command()
-async def user(ctx, member: discord.Member):
-	emb = discord.Embed(title = str(member), description = member.mention, color = member.top_role.color)
-	emb.add_field(name = "ID", value = member.id, inline = False)
-	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
-	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
-	if member.top_role == member.roles[0]:
-		emb.add_field(name = "Highest role", value = member.top_role, inline = False)
-	else:
-		emb.add_field(name = "Highest role", value = member.top_role.mention, inline = False)
-	
-	emb.set_thumbnail(url = member.avatar_url)
-	emb.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
-	await ctx.send(embed = emb)
 	
 token = os.environ.get('BOT_TOKEN')
 
