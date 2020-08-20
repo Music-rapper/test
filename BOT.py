0a9b5ffb6be2eecd8957e8848f8523a3a4452830
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-20 16:17:03 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -9,6 +9,26 @@ Bot = commands.Bot(command_prefix = '-')
 @Bot.command()
 async def hello(ctx):
     	await ctx.send(f"Hello {ctx.author.mention}")
+
+@Bot.command()
+async def user(ctx, member: discord.Member):
+    	emb = discord.Embed(title = str(member), description = member.mention, color = member.top_role.color)
+    	emb.add_field(name = "ID", value = member.id, inline = False)
+    	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
+    	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
+    	if member.top_role == member.roles[0]:
+        	emb.add_field(name = "Highest role", value = member.top_role, inline = False)
+    	else:
+        	emb.add_field(name = "Highest role", value = member.top_role.mention, inline = False)
+	roles = member.roles
+   	emb.set_thumbnail(url = member.avatar_url)
+	emb.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
+    	await ctx.send(roles, embed = emb)	
+	
+@Bot.event
+async def on_ready():
+	print('Bot is online')
+	await Bot.change_presence(status = discord.Status.dnd, activity = discord.Game('Playing with developer'))
 	
 token = os.environ.get('BOT_TOKEN')
 
