9e35112e597d8f4b58c4c04399f4803500fae917
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-21 11:25:45 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -22,7 +22,7 @@ async def roles(ctx, member: discord.Member):
 @Bot.command()
 async def role(ctx, member: discord.Member):
 	role = member.roles
-	await ctx.send(role)
+	await ctx.send(role[1:2])
 	
 @Bot.command()
 async def user(ctx, member: discord.Member):
