24f614bf9017e76df56393244464ab9a01f26bf9
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-23 16:47:39 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -15,7 +15,7 @@ async def join(ctx):
 		await ctx.send('You need to be in a voice chat to use that')
 	else:
 		client = await voice.channel.connect(timeout = 5)
-		await ctx.send(client.session_id)
+		await ctx.send(client.session_id + ' ' + client.guild)
 
 @Bot.command()
 async def leave(ctx):
