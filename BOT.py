272a4cef5e7983b40909937f3320248006776d7c
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-23 18:30:24 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -8,6 +8,7 @@ prefix = 'r!'
 
 Bot = commands.Bot(command_prefix = prefix)
 
+'''
 @Bot.command()
 async def join(ctx):
 	voice = ctx.author.voice
@@ -25,54 +26,16 @@ async def leave(ctx):
 		guild = ctx.guild
 		voice_client = guild.voice_client
 		await voice_client.disconnect()
-
-@Bot.command()
-async def play(ctx, track: discord.AudioSource):
-	if track == None:
-		await ctx.send('You nedd to write what to play.')
-	else:
-		guild = ctx.guild
-		voice_client = guild.voice_client
-		track = discord.AudioSource
-		voice_client.play(track)
+'''
 		
 @Bot.command()
-async def say(ctx, channel = None, *, word = None):
-	stop = False
-	guild = ctx.guild
-	channel_list = guild.text_channels
-	for i in range(0, len(channel_list)):
-		if channel == channel_list[i].name or channel == channel_list[i].id or channel == channel_list[i].mention:
-			if word == None:
-				stop = True
-				await ctx.send('You don\'t wrote what to say')
-			else:
-				stop = True
-				channel = channel_list[i]
-				await channel.send(word)
-	else:
-			if channel == None:
-				await ctx.send('You don\'t wrote what to say')
-			else:
-				if stop == False:
-					if word == None:
-						await ctx.send(channel)
-					else:
-						await ctx.send(channel + f' {word}')
-
-@Bot.command()
-async def user(ctx, member: discord.Member):
-	emb = discord.Embed(title = str(member), description = member.mention, color = member.top_role.color)
-	emb.add_field(name = "ID", value = member.id, inline = False)
-	emb.add_field(name = "Joined server at", value = str(member.joined_at)[:19], inline = False)
-	emb.add_field(name = "Created account at", value = str(member.created_at)[:19], inline = False)
-	if member.top_role == member.roles[0]:
-		emb.add_field(name = "Highest role", value = member.top_role, inline = False)
-	else:
-		emb.add_field(name = "Highest role", value = member.top_role.mention, inline = False)
-	emb.set_thumbnail(url = member.avatar_url)
-	emb.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
-	await ctx.send(embed = emb)
+async def server(ctx):
+	server = ctx.guild
+	s_e = discord.Embed(title = server.name, description = server.description, color = discord.Color.red())
+	s_e.add_field(name = "ID", value = server.id, inline = False)
+	s_e.set_thumbnail(url = server.icon_url)
+	s_e.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
+	await ctx.send(embed = s_e)
 	
 @Bot.event
 async def on_ready():
