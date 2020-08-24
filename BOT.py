e4c3a37214626312755ab959396b6ae03e5625c8
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-24 14:45:18 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -51,6 +51,8 @@ async def server(ctx):
 	s_e.add_field(name = "Channels", value = channels, inline = False)
 	s_e.add_field(name = "Roles", value = len(server.roles))
 	s_e.add_field(name = "Emojis", value = len(server.emojis))
+	if server.region == 'russia':
+		s_e.add_field(name = "Voice Region", value = ":flag_ru: Russia")
 	s_e.add_field(name = "Voice Region", value = server.region, inline = False)
 	s_e.set_thumbnail(url = server.icon_url)
 	s_e.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
