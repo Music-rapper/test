a6c4c8fe33f17b039993c40b9d0b5fb2ba6e06ac
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-24 09:49:58 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -42,9 +42,8 @@ async def server(ctx):
 	for i in range(0, len(server.members)):
 		if server.members[i].status == discord.Status.online or server.members[i].status == discord.Status.idle or server.members[i].status == discord.Status.dnd:
 			online_members += 1
-	members_online = f'<:online:747121287893352509> {str(online_members)}<:transparent:747346151014924319><:offline:747121312262258768> {str(len(server.members))} Members'
-	s_e.add_field(name = "Members", value = members_online)
-	s_e.add_field(name = "", value = members_offline)
+	members = f'<:online:747121287893352509> {str(online_members)}<:transparent:747346151014924319><:offline:747121312262258768> {str(len(server.members))} Members'
+	s_e.add_field(name = "Members", value = members, inline = False)
 	s_e.set_thumbnail(url = server.icon_url)
 	s_e.set_footer(text = f"Caused by: {str(ctx.author)}", icon_url = ctx.author.avatar_url)
 	await ctx.send(embed = s_e)
