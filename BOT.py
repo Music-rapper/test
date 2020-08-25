bb5f329e590b7f73b45c8bd11d1c4f4812299a10
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-08-25 13:14:57 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -57,7 +57,7 @@ async def channel(ctx, channel = None):
 	c_e.add_field(name = 'NSFW', value = channel.is_nsfw())
 	if channel.topic != None:
 		c_e.add_field(name = 'Topic', value = channel.topic, inline = False)
-	c_e.add_field(name = 'Roles', value = channel.overwrites[ctx.author.roles[0].name])
+	c_e.add_field(name = 'Roles', value = ctx.author.permissions_in(channel))
 	c_e.add_field(name = 'Created at', value = channel.created_at, inline = False)
 	c_e.set_footer(text = f'Caused by: {ctx.author}', icon_url = ctx.author.avatar_url)
 	await ctx.send(embed = c_e)
