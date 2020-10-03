06037eb20b60e754512114145e79627d8468a449
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2020-10-03 20:30:11 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -3,30 +3,13 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import os
 
-bot_prefix = '!'
+bot_prefix = ['!']
 
 Bot = commands.Bot(command_prefix = bot_prefix)
 
-@Bot.event
-async def on_message_edit(before: discord.Message, after: discord.Message):
-	channel = discord.utils.get(before.guild.text_channels, name = 'лог')
-	ed_e = discord.Embed(title = 'Message Edited')
-	await channel.send(before.content)
-	await channel.send(after.content)
-
-#Bot.remove_command('help')
-
-'''
 @Bot.command()
-async def help(ctx, command = None):
-	me = ctx.guild.me
-	h_e = discord.Embed(title = f'{me.name} Bot Commands', color = discord.Color.from_rgb(255, 0, 0))
-	h_e.add_field(name = f'{prefix}avatar', value = 'Give you someone\'s avatar that you can easily download.', inline = False)
-	h_e.add_field(name = f'{prefix}emoji', value = 'Give information about custom emoji.', inline = False)
-	h_e.set_thumbnail(url = me.avatar_url)
-	h_e.set_footer(text = f'Caused by: {str(ctx.author)}', icon_url = ctx.author.avatar_url)
-	await ctx.send(embed = h_e)
-'''	
+async def cytate(ctx, id = discord.Message.id):
+	await ctx.send(id)
 
 @Bot.command()
 async def inrole(ctx, role = None):
@@ -55,6 +38,26 @@ async def on_ready():
 	print('Bot is ready!')
 	await Bot.change_presence(status = discord.Status.dnd, activity = discord.Game('1234567890'))
 	
+'''
+@Bot.event
+async def on_message_edit(before: discord.Message, after: discord.Message):
+	channel = discord.utils.get(before.guild.text_channels, name = 'лог')
+	ed_e = discord.Embed(title = 'Message Edited')
+	await channel.send(before.content)
+	await channel.send(after.content)
+
+Bot.remove_command('help')
+
+@Bot.command()
+async def help(ctx, command = None):
+	me = ctx.guild.me
+	h_e = discord.Embed(title = f'{me.name} Bot Commands', color = discord.Color.from_rgb(255, 0, 0))
+	h_e.add_field(name = f'{prefix}avatar', value = 'Give you someone\'s avatar that you can easily download.', inline = False)
+	h_e.add_field(name = f'{prefix}emoji', value = 'Give information about custom emoji.', inline = False)
+	h_e.set_thumbnail(url = me.avatar_url)
+	h_e.set_footer(text = f'Caused by: {str(ctx.author)}', icon_url = ctx.author.avatar_url)
+	await ctx.send(embed = h_e)
+	
 @Bot.event
 async def on_member_join(member: discord.Member):
 	channel = discord.utils.get(member.guild.text_channels, name = 'bot')
@@ -64,6 +67,7 @@ async def on_member_join(member: discord.Member):
 async def on_member_remove(member: discord.Member):
 	channel = discord.utils.get(member.guild.text_channels, name = 'bot')
 	await channel.send(f'{member.mention} leaved the server')
+'''
 	
 token = os.environ.get('BOT_TOKEN')
 
