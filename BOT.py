c229577f07db6099c4d4c0c4cc5b5d5d0acdd074
Vovan4ik507 <47191127+Vovan4ik507@users.noreply.github.com>
2021-03-19 10:03:48 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -10,7 +10,7 @@ Bot = commands.Bot(command_prefix = bot_prefix)
 @Bot.command()
 async def members(ctx):
 	guild = ctx.guild
-	await ctx.send(guild.owner)
+	await ctx.send(guild.members)
 	
 @Bot.command()
 async def inrole(ctx, role = None):
